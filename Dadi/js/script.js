f460ed34cd6c949c59bb23cd59981de878148d47
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-21 13:07:23 +0100

    Modified Js-Dadi

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -6,37 +6,39 @@ const wrapperElement = document.querySelector('div.wrapper');
 
 const writePlayerNum = document.createElement ('p');
 writePlayerNum.className = 'first-two-p'
+wrapperElement.appendChild(writePlayerNum);
 
 let playerNumber;
 const playerBtnElement = document.querySelector ('button#num-player');
 playerBtnElement.addEventListener ('click', function () {
     playerNumber = Math.round ((Math.random()*5) +1);
     writePlayerNum.innerHTML = `Il numero generato dal player è: ${playerNumber}`;
-    wrapperElement.appendChild(writePlayerNum);
 })
 
 
 
 const writeComputerNum = document.createElement ('p');
 writeComputerNum.className = 'first-two-p'
+wrapperElement.appendChild(writeComputerNum);
 
 let computerNumber;
 const computerBtnElement = document.querySelector ('button#num-computer');
 computerBtnElement.addEventListener ('click', function () {
     computerNumber = Math.round ((Math.random()*5) +1);
     writeComputerNum.innerHTML = `Il numero generato dal computer è: ${computerNumber}`;
-    wrapperElement.appendChild(writeComputerNum);
 })
 
 
 const writeWinner = document.createElement ('p');
+wrapperElement.appendChild(writeWinner);
 
 const winnerBtnElement = document.querySelector ('button#winner');
 winnerBtnElement.addEventListener ('click', function () {
-    wrapperElement.appendChild(writeWinner);
     if (playerNumber > computerNumber) {
         writeWinner.innerHTML = 'Il vincitore è il player';
     } else if (playerNumber < computerNumber) {
         writeWinner.innerHTML = 'Il vincitore è il computer';
+    } else {
+        writeWinner.innerHTML = 'Parità';
     }
 })
