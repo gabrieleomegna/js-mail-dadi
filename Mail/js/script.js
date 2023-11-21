a7433916fffc841d6ab6c5afba168058ee2f5d59
gabrieleomegna <gabrieleomegna01@gmail.com>
2023-11-21 15:50:17 +0100

    Implemented script.js of Js-Mail

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,9 +1,24 @@
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (createvi una piccola lista di email),
 // stampa un messaggio appropriato sull’esito del controllo.
 
+const emailArray = [
+    'pincopallino@bellamail.com', 
+    'ginoginetti@bellamail.com',
+    'topolino@bellamail.com', 
+    'paperino@bellamail.com'
+];
 const userEmail = prompt ('Write your email');
-const emailArray = ['pincopallino@bellamail.com', 'ginoginetti@bellamail.com', 'topolino@bellamail.com', 'paperino@bellamail.com'];
+let isMailFound = false;
 
-if (`$(userEmail.value)` === `$emailArray[0]`) {
-    console.log ('ciao');
+
+for (let i = 0; i < emailArray.length; i++) {
+    if (userEmail == emailArray[i]) {
+        isMailFound = true;
+    }
 }
+if (isMailFound) {
+    console.log ('Email has been verified, access guaranteed');
+} else {
+    console.log ('Unrecognized email, access denied');
+}
+
